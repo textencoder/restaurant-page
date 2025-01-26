8bbd678c59062d890693393e54c93e7ed6904428
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-25 18:50:08 -0600

    added photo and basic global styles

--- src/home.js
+++ src/home.js
@@ -1,15 +1,16 @@
 const section = document.createElement('section');
 const headline = document.createElement('h1');
 const description = document.createElement('p');
-const image = document.createElement('div');
+const image = document.createElement('img');
 const addressContainer = document.createElement('div');
 const addressOne = document.createElement('p');
 const addressTwo = document.createElement('p');
 
 section.id = 'home-section';
 image.id = 'img';
+image.src = 'https://cdn.5280.com/2023/03/Boulder-New-Pearl_pearl-street-hero-1536x1152.jpg'
 headline.innerText = "Bold Flavors. Thoughtful Dining.";
-description.innerText = "Modern American with a twist of global influences, featuring a farm-to-table menu emphasizing seasonal and locally-sourced ingredients.";
+description.innerText = "Crimson Fork is a modern American cafe with a twist of global influences, featuring a farm-to-table menu emphasizing seasonal and locally-sourced ingredients.";
 addressOne.innerText = "548 Aspen Grove Lane";
 addressTwo.innerText = "Boulder, Colorado 80301";
 
--- src/index.js
+++ src/index.js
@@ -14,19 +14,33 @@ set_home: for (let value of Object.values(Home)) {
     document.querySelector('#content').append(value);
 }
 
+homeButton.style.fontWeight = '700';
+
 homeButton.addEventListener('click', () => {
     content.innerHTML = '';
     for (let value of Object.values(Home)) {
         document.querySelector('#content').append(value);
     }
+    document.querySelectorAll('button').forEach(b => {
+        b.style.fontWeight = '500';
+    })
+    homeButton.style.fontWeight = '700';
 })
 
 menuButton.addEventListener('click', () => {
     content.innerHTML = '';
     appendTest();
+    document.querySelectorAll('button').forEach(b => {
+        b.style.fontWeight = '500';
+    })
+    menuButton.style.fontWeight = '700';
 })
 
 aboutButton.addEventListener('click', () => {
     content.innerHTML = '';
     aboutSection();
+    document.querySelectorAll('button').forEach(b => {
+        b.style.fontWeight = '500';
+    })
+    aboutButton.style.fontWeight = '700';
 })
